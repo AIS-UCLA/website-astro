757ed938ed8f3dbfd43edc970a9e7777e281a218
Kushal Baranwal <imkushal01@gmail.com>
2026-04-03 14:51:45 -0700

    added company logos for more flexing

---
+++ src/components/CompanyLogos.tsx
@@ -0,0 +1,17 @@
+export default function CompanyLogos() {
+  return (
+    <div className="grid grid-cols-3 gap-12 items-center justify-center">
+      <img src="/assets/logos/anthropic.svg" />
+      <img src="/assets/logos/mats.svg" />
+      <img src="/assets/logos/metr.svg" />
+      <img src="/assets/logos/aifutures.png" />
+      <img src="/assets/logos/aipolicynetwork.png" />
+      <img src="/assets/logos/apollo.png" />
+      <img src="/assets/logos/equistamp.png" />
+      <img src="/assets/logos/forethought.svg" />
+      <img src="/assets/logos/IAPSlogo.webp" />
+      <img src="/assets/logos/spar.jpeg" />
+      <img src="/assets/logos/uchicagoxrisklab.png" />
+    </div>
+  );
+}
